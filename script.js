2bda65898899a10d582fe8babdc05ae197a8f8cc
dorizugno <dorivital047@gmail.com>
2024-08-16 11:10:28 -0300

    javascript introdução

---
+++ script.js
@@ -0,0 +1,16 @@
+// alerta na tela
+alert("Boa tarde, Neimá")
+
+
+// console do navegador
+
+console.log("Teste de console");
+
+// inclui 
+document.getElementById("demo").innerHTML = "Abrir poço com vc, Neimá";
+
+
+//perguntando ao usuário
+
+let nome= prompt("Digite seu nome");
+alert("Olá  "+nome+", tudo bem ?")
